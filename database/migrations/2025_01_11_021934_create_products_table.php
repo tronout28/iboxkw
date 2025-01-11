8bef237e027f3b930b2a0dbde89547d2556e78f6
tronout28 <fatihabdurahman28@gmail.com>
2025-01-11 10:48:18 +0700

    config roles

---
+++ database/migrations/2025_01_11_021934_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('price');
+            $table->string('image')->nullable();
+            $table->enum('requested',['non-accepted','accepted','rejected'])->default('non-accepted');
+            $table->enum('status', ['active', 'inactive'])->default('inactive');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
