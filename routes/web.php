61373d74aac6228704a05f2c5f8d34244ed65dd2
tronout28 <fatihabdurahman28@gmail.com>
2025-01-17 17:17:31 +0700

    addoke ok

--- routes/web.php
+++ routes/web.php
@@ -100,3 +100,19 @@ Route::get('/images-product/{filename}', function ($filename) {
 
     return $response;
 });
+
+Route::get('/images-artikel/{filename}', function ($filename) {
+    $path = public_path('images-artikel/'.$filename);
+
+    if (! File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header('Content-Type', $type);
+
+    return $response;
+});
